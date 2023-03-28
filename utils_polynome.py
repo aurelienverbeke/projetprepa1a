8fab0d7c7f617b87a03efca4fda68d107cc4ffc9
Brice-Vergnou <86613710+Brice-Vergnou@users.noreply.github.com>
2023-03-28 10:47:10 +0200

    Ajouter la fonction monome

--- utils_polynome.py
+++ utils_polynome.py
@@ -12,6 +12,19 @@ Authors:
 
 # Brice
 
+def monome(n):
+  """
+    Renvoie une liste de coefficients par ordre de puissances croissantes 
+    correspondant au monome X^n  
+    Args:
+        - n (int >=0 ): degré du monome
+    Returns :   
+        list : liste de coefficients du monômes
+    Exemple :
+      monome(4)
+      >>> [0, 0, 0, 0, 1]
+    """
+  return [0]*(n) + [1]
 
 # Raphael
 
