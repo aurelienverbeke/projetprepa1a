3c1a09da0f767c97b4df02fe4ed193be20ef234c
Aurelien VERBEKE <aurelienv31@gmail.com>
2023-03-28 10:50:22 +0200

    Implementation de la fonction somme

--- utils_polynome.py
+++ utils_polynome.py
@@ -8,7 +8,32 @@ Authors:
 """
 
 # Aurelien
+def somme(P1, P2):
+    """
+        Renvoie la somme de deux polynomes
+        Args:
+            - P1, P2 (list): deux polynomes a additionner
+        Returns:
+            list: somme des deux polynomes
+        Exemple:
+            >>> somme([4, 3, -2], [3, 6, 2])
+            [7, 9]
 
+    """
+
+    longueurMax = max([len(P1), len(P2)])
+    somme = [0] * longueurMax
+    
+    for i in range(longueurMax):
+        if i < len(P1):
+            somme[i] += P1[i]
+        if i < len(P2):
+            somme[i] += P2[i]
+    
+    while somme[-1] == 0:
+        somme.pop()
+    
+    return somme
 
 # Brice
 
