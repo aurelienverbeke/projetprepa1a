0067acd7eb35c5ef0e278848c54aa6098e6b7291
Raphaël Bourgouin <raph.bourgouin@free.fr>
2023-04-11 19:54:50 +0200

    L'exercice 3 renvoie des réponses propres maintenant

--- exercice_3.py
+++ exercice_3.py
@@ -6,7 +6,7 @@ Authors:
 """
 
 from fractions import Fraction as F
-from utils_polynome import derive_polyn, pgcd, division, evalue, produit, puissance
+from utils_polynome import derive_polyn, pgcd, division, evalue, produit, puissance, polyn_to_str
 from math import sqrt
 
 R = [F(2), F(-7), F(-6), F(34), F(10), F(-63), F(-22), F(44), F(24)]
@@ -15,10 +15,10 @@ R = [F(2), F(-7), F(-6), F(34), F(10), F(-63), F(-22), F(44), F(24)]
 R_prime = derive_polyn(R)
 denominateur = pgcd(R, R_prime)
 Q = division(R, denominateur)[0]
-print(Q)
+print(f"Question 1: R/(R^R') = {polyn_to_str(Q)}\n")
 
 # -1 est bien une racine de Q
-print(evalue(Q, -1))
+print(f"Question 2: Q(-1) = {evalue(Q, -1)}\n")
 
 # On calcule les racines restantes de Q
 T = division(Q, [1, 1])[0] # C'est un polynome de second degré
@@ -28,7 +28,7 @@ racine1 = F(-1)
 # On utilise la formule
 racine2 = F(int(-T[1] + sqrt(delta)), 2*T[2])
 racine3 = F(int(-T[1] - sqrt(delta)), 2*T[2])
-print(racine1, racine2, racine3)
+print(f"            Les racines de Q sont {racine1}, {racine2} et {racine3}\n")
 
 multiplicite_racine1 = 1
 multiplicite_racine2 = 1
@@ -54,13 +54,18 @@ while r1 == 0 or r2 == 0 or r3 == 0:
         multiplicite_racine3 += 1
         r3 = evalue(derive_R, racine3)
 
-print(multiplicite_racine1, multiplicite_racine2, multiplicite_racine3)
+print(f"Question 3: Les racines de R sont les mêmes que celles de Q, donc les racines de R sont {racine1}, {racine2} et {racine3}\n"
+      f"            De plus:\n"
+      f"            - La racine {racine1} est de multiplicité {multiplicite_racine1}\n"
+      f"            - La racine {racine2} est de multiplicité {multiplicite_racine2}\n"
+      f"            - La racine {racine3} est de multiplicité {multiplicite_racine3}\n")
 
 pol_r1 = puissance([-racine1, F(1)], multiplicite_racine1)
 pol_r2 = puissance([-racine2, F(1)], multiplicite_racine2)
 pol_r3 = puissance([-racine3, F(1)], multiplicite_racine3)
 
 # On trouve le polynome unitaire associé aux racines
-R_exp = produit(produit(pol_r1, pol_r2), pol_r3)
-print(produit(R_exp, [24])) # On multiplie R_exp par le coefficient dominant de R
-print(R)
+R_uni = produit(produit(pol_r1, pol_r2), pol_r3)
+R_exp = produit(R_uni, [24]) # On multiplie R_exp par le coefficient dominant de R
+print(f"Question 4: On trouve en multipliant les racines le polynome {polyn_to_str(R_uni)}\n"
+      f"            Donc en multipliant par dom(R), on retrouve bien R ({polyn_to_str(R_exp)})\n")
