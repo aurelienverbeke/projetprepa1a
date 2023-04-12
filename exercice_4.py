cb06b97a20cf865108c7e8812f983da6bc4c49a2
Raphaël Bourgouin <raph.bourgouin@free.fr>
2023-04-13 01:54:03 +0200

    Réécriture complète de racines_polyn

--- exercice_4.py
+++ exercice_4.py
@@ -8,21 +8,31 @@ Authors:
 from utils_polynome import derive_polyn, division, pgcd, deg, unitaire, evalue, polyn_to_str
 from fractions import Fraction as F
 import numpy as np
-from math import inf
+import numpy.polynomial.polynomial as poly
 from time import time
 from exercice_1 import tchebychev
 
-def Newton(f, a, b, epsilon):
+def Newton(f, a, b, k):
     """
     Trouve le zéro de f présent entre a et b avec une précision de epsilon
     Args:
          - f (lambda) : la fonction pour laquelle on cherches le zéro
          - a (float) : le minimum de l'intervalle de recherche
          - b (float) : le maximum de l'intervalle de recherche
-         - epsilon (float) : la précision
+         - k (int) : le nombre de décimales de précision
     Returns:
         float : le zéro de f
     """
+    epsilon = 10**-k
+
+    # On réduit l'intervalle afin de faire converger la suite plus rapidement
+    while abs(a - b) > 1:
+        m = (a + b) / 2
+        if f(a) * f(m) <= 0:
+            b = m
+        else:
+            a = m
+
     # Sachant que souvent a et b seront des zéros d'une des dérivées
     # On préfèrera prendre x et y au milieu de l'intervalle afin de s'en éloigner
     x = (a+b)/2
@@ -30,93 +40,102 @@ def Newton(f, a, b, epsilon):
 
     # On utilise le principe de la méthode de Newton
     while abs(y - x) > epsilon:
+        # Si y sort de l'intervalle, on applique une dichotomie et on recommence
+        if y < a or y > b:
+            m = (a+b)/2
+            if f(a)*f(m) <= 0:
+                b = m
+            else:
+                a = m
+            x = (a+b)/2
+            y = x + 1e-8
         tmp = y
         y = y - f(y)*(y-x)/(f(y)-f(x))
         x = tmp
     return y
 
-def racines_polyn(P, precision=1e-8, reduire=True, renvoyer_intervalles=False):
+def racines_polyn_faible_degree(Q):
     """
-    Revoie la liste des racines d'un polynome
+    Renvoie les racines d'un polynome de degré au plus 2
     Args:
-        - P (list) : le polynome pour lequel on veut les racines
-        - precision : la precision des racines
-        - reduire : True si l'on veut reduire le polynome avec Q = P/pgcd(P, P_prime), le laisser à false rend très souvent
-        la fonction plus rapide, mais cela peut engendrer des erreurs d'approximation
-        - renvoyer_intervalles : Doit être laissé à False
+        - P (list) : Le polynome pour lequel on veut les racines
+    Return:
+        list: La liste des racines de P
     """
 
-    # Si le polynome est constant, on considère qu'il n'a pas de racines
-    if deg(P) == - inf:
+    if deg(Q) <= 0:
         return []
-    if deg(P) == 0:
-        return []
-
-    if reduire:
-        # On réduit le polynôme pour avoir un polynôme plus petit mais avec les même racines
-        P = [F(x) for x in P]
-        P_prime = derive_polyn(P)
-        Q = division(P, pgcd(P, P_prime))[0]
-
-        Q = unitaire([float(x) for x in Q])
-    else:
-        Q = unitaire([float(x) for x in P])
-
-# ------------------CAS DE BASE-------------------
-    # Si le polynôme est de degré 1 ou 2 on utilise les formules que l'on connait pour revoyer les racines
     if deg(Q) == 1:
         return [-Q[0]/Q[1]]
     if deg(Q) == 2:
-        delta = Q[1]**2-4*Q[2]*Q[0]
+        delta = Q[1] ** 2 - 4 * Q[2] * Q[0]
         if delta > 0:
-            x1 = (-Q[1]+np.sqrt(delta))/(2*Q[2])
-            x2 = (-Q[1]-np.sqrt(delta))/(2*Q[2])
+            x1 = (-Q[1] + np.sqrt(delta)) / (2 * Q[2])
+            x2 = (-Q[1] - np.sqrt(delta)) / (2 * Q[2])
             # Il faut ordoner les racines
             if x1 > x2:
                 return [x2, x1]
             else:
                 return [x1, x2]
         if delta == 0:
-            return [-Q[1]/(2*Q[2])]
+            return [-Q[1] / (2 * Q[2])]
         else:
             return []
 
-# ------------------RECURSIVITE-------------------
-    # On récupère les racines de toutes les dérivées du polynôme
+    raise ValueError(f"Le polynome doit etre de degree inferieur ou egal a 2, or ici Q={Q}")
 
-    # Même si les dérivées premières et secondes sont suffisantes pour garantir la convergence, si l'on ne considère que
-    # les racines de la dérivée première et seconde, il arrive que la suite de Newton sorte de l'intervalle
-    # la même chose arrive si l'on considère aussi les racines de la dérivée troisième
-    racines_derivees = racines_polyn(derive_polyn(Q), reduire=False, renvoyer_intervalles=True)
+def racines_polyn(P, nombre_decimales=8):
+    """
+    Renvoie la liste des racines d'un polynome
+    Args:
+        - P (list) : le polynome pour lequel on veut les racines
+        - nombre_decimales (int) : Le nombre de chiffre après la virgule de précision
+    Returns:
+        list: La liste des racines de P
+    """
 
-    # On calcule l'intervalle dans lequel les racines sont situées
-    mu = max(1, sum([abs(x) for x in Q[:-1]]))
+    # Si le polynome est constant, on considère qu'il n'a pas de racines
+    if deg(P) <= 0:
+        return []
 
-    # On obtient un subdivision de l'intervalle [-mu, mu] pour lequel il y a au maximum une racine dans chaque intervalle
-    # de cette subdivision
-    intervalles = [-mu-1e-3] + racines_derivees + [mu+1e-3]
+    # On réduit le polynome en un polynomes Q ayant les memes racines que P mais uniquement en racines simples
+    P = [F(x) for x in P]
+    Q = division(P, pgcd(P, derive_polyn(P)))[0]
+    Q = unitaire([float(x) for x in Q])
 
-    racines = []
-    f = lambda x: evalue(Q, x)
-    for i in range(len(intervalles)-1):
-        a = intervalles[i]
-        b = intervalles[i+1]
+    # Si le degré est de 1 ou 2, on utilise directement les formules que l'on connait
+    if deg(P) <= 2:
+        return racines_polyn_faible_degree(Q)
 
-        # On ajoute si nécéssaire les racines des dérivées
-        if renvoyer_intervalles and i > 0:
-            racines.append(a)
+    # On calcule un encadrement de toutes les racines
+    mu = max(1, sum([abs(Q[x]) for x in range(len(Q)-1)]))
 
-        # On applique un TVI afin de vérifier l'existence d'un racine dans l'intervalle
-        if f(a)*f(b) <= 0:
-            # On calcule la racine grâce à la méthode de Newton
-            racines.append(Newton(f, a, b, precision))
+    # On calcule les dérivées successives de Q jusqu'à tomber sur un polynome constant
+    derivees = [Q]
+    while deg(derivees[-1]) > 0:
+        derivees.append(unitaire(derive_polyn(derivees[-1])))
+        # On rend tous les polynomes unitaires afin d'éviter les erreurs qui peuvent survenir
+        # avec des flotants trop grands lorsqu'on évalue
 
-    # On arrondis les racines finales à la précision demandée
-    # les décimales après ne seront pas correctes, cela ne sert à rien de les garder
-    if not renvoyer_intervalles:
-        racines = [round(x, int(-np.log10(precision))) for x in racines]
-    return racines
+    intervalles = [-mu, mu]
+    # On remonte la liste des dérivée
+    # En utilisant le fait entre 2 racines successives de la dérivée il existe au plus une racine
+    for idx in range(len(derivees)-1, -1, -1):
+        f = lambda x: evalue(derivees[idx], x)
 
+        if deg(derivees[idx]) <= 2:
+            racines = racines_polyn_faible_degree(derivees[idx])
+        else:
+            racines = []
+            for i in range(len(intervalles)-1):
+                # On vérifie la présence d'un racine avec un TVI
+                if f(intervalles[i])*f(intervalles[i+1]) <= 0:
+                    # On applique Newton
+                    racines.append(Newton(f, intervalles[i], intervalles[i+1], nombre_decimales))
+
+        intervalles = [-mu] + racines + [mu]
+
+    return intervalles[1:-1]
 
 def racines_polyn_fich(ftxt):
     """
@@ -138,8 +157,16 @@ def racines_polyn_fich(ftxt):
 
 
 if __name__ == "__main__":
+    """
     P = [F(2), F(-7), F(-6), F(34), F(10), F(-63), F(-22), F(44), F(24)]
     racines = racines_polyn(P)
     print(f"Les racines de {polyn_to_str(P)} sont : {racines[0]}, {racines[1]} et {racines[2]}\n")
     print(f"Les racines de polynomes de polyn.txt sont:")
     racines_polyn_fich("polyn.txt")
+    """
+    for i in range(1, 50):
+        n_roots = [round(float(x), 8) for x in list(poly.Polynomial(tchebychev(i)).roots()) if complex(x).imag == 0]
+        roots = [round(x, 8) for x in racines_polyn(tchebychev(i))]
+        print(i, n_roots == roots)
+        print(n_roots)
+        print(roots)
