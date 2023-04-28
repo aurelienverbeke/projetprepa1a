d362221b370f0a68236aba1af4addb5c00218184
Raphaël Bourgouin <raph.bourgouin@free.fr>
2023-04-28 16:55:24 +0200

    Reformatage des réponses et correction d'un bug

--- exercices_fichiers_raphael.py
+++ exercices_fichiers_raphael.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # 3
 def NbCar(file):
-    with open(f"fichiers/{file}", "r") as f:
+    with open(f"{file}", "r") as f:
         nb = len(f.read().strip())
 
     return nb
@@ -20,7 +20,7 @@ def NbCar(file):
 
 # 4
 def NbLigne(file):
-    with open(f"fichiers/{file}", "r") as f:
+    with open(f"{file}", "r") as f:
         nb = len(f.readlines())
 
     return nb
@@ -28,7 +28,7 @@ def NbLigne(file):
 
 # 5
 def NbOccurences(file, c):
-    with open(f"fichiers/{file}", "r") as f:
+    with open(f"{file}", "r") as f:
         occurences = 0
         for car in "".join(f.readlines()):
             if car == c:
@@ -39,26 +39,30 @@ def NbOccurences(file, c):
 
 # 6
 def MAJ(file):
-    with open(f"fichiers/{file}", "r") as f:
+    with open(f"{file}", "r") as f:
         lignes = f.read()
 
     lignes = lignes.upper()
 
-    with open(f"fichiers/MAJ_{file}", "w") as f:
+    with open(f"MAJ_{file}", "w") as f:
         f.write(lignes)
 
 
-print(NbCar("coucou"))
-print(NbLigne("coucou"))
-print(NbOccurences("coucou", "e"))
+with open("coucou", "w") as f:
+    f.write("wow un fichier texte avec du texte dedans\nIncroyable non?")
+
+print(f"Le fichier coucou possède {NbCar('coucou')} caractères")
+print(f'Il possède {NbLigne("coucou")} lignes')
+print(f'Et il y a {NbOccurences("coucou", "e")} e dans le fichier coucou')
 MAJ("coucou")
 
 # Exercice 6.2
 
-with open(f"fichiers/courbe.txt", "r") as f:
-    liste_points_bruts = f.read().replace("\n", " : ").split(" : ") # Récupère le texte et le sépare avec " : "
-    liste_points_bruts = [x.split(",") for x in liste_points_bruts] # Convertis la liste de caractère en liste de couples de la forme ("x", "y")
-    points = [(float(x[0]), float(x[1])) for x in liste_points_bruts] # Convertis les coordonnées de str à float
+with open(f"courbe.txt", "r") as f:
+    liste_points_bruts = f.read().replace("\n", " : ").split(" : ")  # Récupère le texte et le sépare avec " : "
+    liste_points_bruts = [x.split(",") for x in
+                          liste_points_bruts]  # Convertis la liste de caractère en liste de couples de la forme ("x", "y")
+    points = [(float(x[0]), float(x[1])) for x in liste_points_bruts[:-1]]  # Convertis les coordonnées de str à float
 
 x = [i[0] for i in points]
 y = [i[1] for i in points]
